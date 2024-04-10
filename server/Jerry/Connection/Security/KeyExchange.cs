d9cbf65318f784ff4e4aba7096d3452ca22a417d
Veronika Klapova <veronika.klapova@gmail.com>
2024-04-10 10:09:11 +0200

    refactor(server/security): remove test data

--- server/Jerry/Connection/Security/KeyExchange.cs
+++ server/Jerry/Connection/Security/KeyExchange.cs
@@ -19,7 +19,6 @@ internal class KeyExchange
     //32B, 256bit
     private const int AGREEMENT_SIZE = 32;
     private const int STRENGTH = AGREEMENT_SIZE * 8;
-
     private readonly X25519KeyPairGenerator generator;
 
     public KeyExchange()
@@ -76,11 +75,4 @@ internal class KeyExchange
         }
     }
 
-    public static void GetMasterDebugAgreement()
-    {
-        //var ourKey = new byte[] { 57, 206, 22, 169, 151, 231, 235, 74, 114, 176, 26, 172, 245, 43, 121, 77, 173, 250, 244, 239, 225, 175, 11, 11, 55, 21, 147, 168, 245, 15, 170, 53 };
-        //var ourNonce = new byte[] { 178, 9, 41, 7, 32, 205, 169, 198, 8, 141, 3, 165, 244, 177, 119, 239, 149, 107, 81, 87, 204, 140, 159, 200, 116, 252, 204, 20, 251, 239, 205, 27 };
-        //var counterpartKey = new byte[] { 43, 121, 77, 169, 151, 231, 235, 74, 114, 176, 26, 172, 245, 43, 121, 77, 173, 250, 244, 239, 225, 175, 11, 11, 55, 21, 147, 168, 245, 15, 170, 53 };
-        //var counterpartNonce = new byte[] { 179, 10, 42, 7, 32, 205, 169, 198, 8, 141, 3, 165, 244, 177, 119, 239, 149, 107, 81, 87, 204, 140, 159, 200, 116, 252, 204, 20, 251, 239, 205, 27 };
-    }
 }
